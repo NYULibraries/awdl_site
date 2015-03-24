abab589c63b91b94254f90c2c6730b258443f697
Laura Henze <laurahenze@gmail.com>
2015-03-24 16:24:50 -0400

    NOJS removal outside of tabs rendering

--- source/js/front.js
+++ source/js/front.js
@@ -7,28 +7,22 @@ YUI().use(
   , function(Y) {
   
     'use strict';
+
+    var html = Y.one('html');
+
+    if ( html.hasClass('nojs') ) {
+      html.removeClass('nojs');
+    }
     
     var tabs = new Y.TabView({ srcNode: '#YUItabs' }).render();
     
     tabs.on('render', function () {
-    
-        var html = Y.one('html');
-    
-        if ( html.hasClass('nojs') ) {
-      
-            var anim = new Y.Anim({
-                node: this.get("srcNode"),
-                to: { opacity: 1 },
-                duration: 0.3
-            });
-
-            html.removeClass('nojs');
-
-            anim.run();
-
-        }
-
+      var anim = new Y.Anim({
+        node: this.get("srcNode"),
+        to: { opacity: 1 },
+        duration: 0.3
+      });
+      anim.run();
     });
-
   }
 );
