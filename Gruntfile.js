298b4187fc36e1393eee7f8178623a252fc8d16c
Alberto Ortiz <aortiz@nyu.edu>
2015-03-30 15:28:11 -0400

    Allow node_modules for DLTS JS modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,22 @@
-module.exports = function (grunt) {
+module.exports = function ( grunt ) {
 
   var _ = require('underscore');
 
   var pkg = grunt.file.readJSON('package.json');
-    
+
   var transform = require('transform');
-  
-  var tasks_configurations = require('./taskConfigurations');  
-    
+
+  var configuration = require('./Gruntconfigurations');  
+
   /** project configuration */
   grunt.initConfig({
     pkg: pkg,
-    curl: tasks_configurations.curl(),
-    clean: tasks_configurations.clean(),
-    copy: tasks_configurations.copy(),
-    uglify: tasks_configurations.uglify(),
-    sass: tasks_configurations.sass(),
-    watch: tasks_configurations.watch()
+    curl: configuration.curl(),
+    clean: configuration.clean(),
+    copy: configuration.copy(),
+    uglify: configuration.uglify(),
+    sass: configuration.sass(),
+    watch: configuration.watch()
   });
   
   /** load modules and tasks */
@@ -30,27 +30,26 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
     
   /** Reads the RSS feed from "ISAW Library Blog". @TODO: ideally this will be a sub-module */
-  grunt.registerTask('isawLibraryBlog', 'isawLibraryBlog', function() {
+  grunt.registerTask('isawLibraryBlog', 'isawLibraryBlog', function () {
     var done = this.async();
     var request = require('request');
-    var fs = require('fs');        
+    var fs = require('fs');
     var xml2js = require('xml2js');
-    var parser = new xml2js.Parser();        
+    var parser = new xml2js.Parser();
     var src = "http://isaw.nyu.edu/library/blog/collector/RSS";
     var dest = __dirname + "/source/json/datasources/isawBlog.json";
     request( src, function (error, response, body) {
       if ( ! error && response.statusCode == 200) {
         parser.parseString( body, function ( err, result ) {
           var blogTitle = result['rdf:RDF']['channel'][0].title[0];
-          var blogLink = result['rdf:RDF']['channel'][0].link[0];                    
+          var blogLink = result['rdf:RDF']['channel'][0].link[0];
           var blogDescription = result['rdf:RDF']['channel'][0].description[0];
           /** this widget only show the first item of the RSS feed */
           var items = result['rdf:RDF'].item.shift();
-          var feed = { 
+          var feed = {
           	title : blogTitle, 
-          	link : blogLink , 
-          	description : 
-          	blogDescription , 
+          	link : blogLink,
+          	description : blogDescription,
           	items : [ items ] 
           } ;
           grunt.file.write( dest , JSON.stringify( feed ) );
@@ -60,7 +59,7 @@ module.exports = function (grunt) {
     });
   });
 
-  grunt.registerTask('writeHTML', 'writeHTML', function() {
+  grunt.registerTask('writeHTML', 'writeHTML', function () {
     /** force task into async mode and grab a handle to the "done" function. */
     var done = this.async();
     /** load configuration */
@@ -79,14 +78,14 @@ module.exports = function (grunt) {
         /** if callback is set, we need to load the JS module and call it */
         if ( ! _.isUndefined ( pages[index].callback )  ) {
           /** load JS module */
-          // var module = require( element.callback ) ;
+          var module = require( element.callback ) ;
           /** 
            * call module with parent configuration so that its possible 
            * to overwrite defaults. Our modules accept a Function callback
            * we pass "transform.html" as the default.
            * 
            */
-          // module[element.callback]( transform.dynamic, { parent_conf : conf } ) ;
+          module[element.callback]( transform.dynamic, { parent_conf : conf } ) ;
         }
         else {
           /** all we need to construct this HTML page it's in the page Object */
@@ -102,4 +101,3 @@ module.exports = function (grunt) {
   grunt.registerTask('default', ['clean', 'copy', 'curl', 'uglify', 'sass', 'isawLibraryBlog', 'writeHTML']);
    
 };
-
