47a39ef465bcd48cd6f9a5ae95d36d2705b391cb
Laura Henze <laurahenze@gmail.com>
2015-04-02 15:52:29 -0400

    New Newsitem layout

--- source/js/front.js
+++ source/js/front.js
@@ -1,28 +1,28 @@
 /* jshint laxcomma: true */
 YUI().use(
-    'node'
-  , 'event'
-  , 'anim'
-  , 'tabview'
-  , function(Y) {
-  
+  'node', 'event', 'anim', 'tabview',
+  function(Y) {
     'use strict';
-
     var html = Y.one('html');
-
-    if ( html.hasClass('nojs') ) {
-      html.removeClass('nojs');
-    }
-    
-    var tabs = new Y.TabView({ srcNode: '#YUItabs' }).render();
-    
-    tabs.on('render', function () {
+    var body = Y.one('body');
+    var tabs = new Y.TabView({
+      srcNode: '#YUItabs'
+    }).render();
+    tabs.on('render', function() {
       var anim = new Y.Anim({
         node: this.get("srcNode"),
-        to: { opacity: 1 },
+        to: {
+          opacity: 1
+        },
         duration: 0.3
       });
       anim.run();
     });
+    if (html.hasClass('nojs')) {
+      html.removeClass('nojs');
+    }
+    if (body.hasClass('io-loading')) {
+      body.removeClass('io-loading');
+    }
   }
 );
