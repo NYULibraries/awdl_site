0772df2e3e107e4e8b1475f5077cd9e7f4308a05
Alberto Ortiz Flores <aortiz@nyu.edu>
2015-04-06 00:40:19 -0400

    Series pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,29 +39,28 @@ module.exports = function ( grunt ) {
         var src = "http://isaw.nyu.edu/library/blog/collector/RSS";
         var dest = __dirname + "/source/json/datasources/isawBlog.json";
         request( src, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                parser.parseString( body, function ( err, result ) {
-                    var blogTitle = result['rdf:RDF']['channel'][0].title[0];
-                    var blogLink = result['rdf:RDF']['channel'][0].link[0];                    
-                    var blogDescription = result['rdf:RDF']['channel'][0].description[0];
+          if (!error && response.statusCode == 200) {
+            parser.parseString( body, function ( err, result ) {
+              var blogTitle = result['rdf:RDF']['channel'][0].title[0];
+              var blogLink = result['rdf:RDF']['channel'][0].link[0];                    
+              var blogDescription = result['rdf:RDF']['channel'][0].description[0];
                     
-                    /** this widget only show the first item of the RSS feed */
-                    var items = result['rdf:RDF'].item.shift();
+              /** this widget only show the first item of the RSS feed */
+              var items = result['rdf:RDF'].item.shift();
                     
-                    /**
-                     * See: https://jira.nyu.edu/browse/AWDL-144?focusedCommentId=53481&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-53481
-                     *  FullMonthName DayOfMonth, FourDigitYear
-                     *  No zero filling for the Day of Month. 
-                     */
-                    items.date = dateFormat( items['dc:date'][0], "mmmm dS, yyyy") ;
-                    items.title = items.title[0] ;                    
-                    items.link = items.link[0] ;                                        
-                    var feed = { title : blogTitle , link : blogLink , description : blogDescription , items : [ items ] } ;
-                    grunt.file.write( dest , JSON.stringify( feed ) );
-                    done();
-                });
-            }
-        })
+              /**
+               * See: https://jira.nyu.edu/browse/AWDL-144?focusedCommentId=53481&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-53481
+               *  - FullMonthName DayOfMonth, FourDigitYear
+               *  - No zero filling for the Day of Month. 
+               */
+              items.date = dateFormat( items['dc:date'][0], "mmmm dS, yyyy") ;
+              items.title = items.title[0] ;                    
+              items.link = items.link[0] ;                                        
+              grunt.file.write ( dest , JSON.stringify ( { title : blogTitle , link : blogLink , description : blogDescription , items : [ items ] } ) ) ;
+              done();
+            });
+          }
+        });
   });
 
   grunt.registerTask('default', ['clean', 'copy', 'curl', 'uglify', 'sass', 'isawLibraryBlog', 'writeHTML']);
