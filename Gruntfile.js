05a42a04f3e3e8598efb7fd67bd02a63e462d1e9
Alberto Ortiz Flores <aortiz@nyu.edu>
2015-04-16 12:25:07 -0400

    Added htmlminify

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,6 @@ module.exports = function ( grunt ) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass'); // remove later on
   grunt.loadNpmTasks('grunt-contrib-compass');  
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-writeHTML');
