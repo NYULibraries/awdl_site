a7e0e22bf51ef6bdb18df480767641070f0b3bd4
Alberto Ortiz <aortiz@nyu.edu>
2015-09-22 14:49:07 -0400

    Update cron.js

--- cron.sh
+++ cron.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# crontab -e 
+# 0 8 * * * /app/cron.sh
+
+NOW=`date`
+
 TODAY=`date +%Y%m%d`
 
 PROJECT_DIR=/www/sites/awdl
@@ -10,11 +15,17 @@ BACKUPS_DIR=${PROJECT_DIR}/backups
 
 EPOCH=`stat -c '%Y' ${BUILD_DIR}`
 
+echo ${NOW} > ${BUILD_DIR}/${TODAY}.out
+
 mv ${BUILD_DIR} ${BACKUPS_DIR}/${EPOCH}
 
-grunt --no-color --base ${PROJECT_DIR} > ${PROJECT_DIR}/logs/${TODAY}.out
+/usr/local/bin/grunt --no-color --gruntfile ${PROJECT_DIR}/Gruntfile.js --base ${PROJECT_DIR} >> ${PROJECT_DIR}/${TODAY}.out
+
+mv ${PROJECT_DIR}/${TODAY}.out ${BUILD_DIR}/${TODAY}.out 
 
 chmod 2775 -R ${BUILD_DIR}
+
+chmod 550 ${BUILD_DIR}/${TODAY}.out
 
 exit 0
 
