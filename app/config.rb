01628bc5c0b592d8d3efa86fe5b87ee61a951180
Laura Henze <laura.henze@nyu.edu>
2018-06-15 16:12:03 -0400

    Sass configs

---
+++ app/config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = '../build/css'
+sass_dir = 'sass'
+images_dir = '../build/images'
+javascripts_dir = '../build/js'
+relative_assets = true
+line_comments = false
+sourcemap = false
+output_style = :compressed
