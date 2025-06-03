371e97998fa43b2460de3562a52318902985cf37
Damon Chu <dc3800@nyu.edu>
2025-06-03 04:19:29 -0400

    server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import rehypeExternalLinks from 'rehype-external-links';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	output: 'hybrid',
+	output: 'server',
 	adapter: awsAmplify({
 		imageService: true
 	}),
