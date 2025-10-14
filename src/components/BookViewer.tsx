0c496139a8a0efb6554117d3d326d97fcd7df274
Damon Chu <dc3800@nyu.edu>
2025-10-14 15:07:23 -0400

    iframe events

--- src/components/BookViewer.tsx
+++ src/components/BookViewer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import BookviewerPlaceholder from './BookViewerPlaceholder';
 import calculateAvailableHeight from './Util/getAvailableHeight';
 
@@ -10,30 +10,68 @@ interface BookProps {
 
 const Book: React.FC<BookProps> = (props) => {
 	const { title, viewer, pid } = props;
-	const [isLoaded, setIsLoaded] = useState(false);
-	const [iFrameHeight, setIFrameHeight] = useState(calculateAvailableHeight());
+	const [isLoaded, setIsLoaded] = useState<boolean>(false);
+	const [iFrameHeight, setIFrameHeight] = useState<number>(calculateAvailableHeight());
+	const iFrameRef = useRef<HTMLIFrameElement>(null);
 
 	useEffect(() => {
 		const handleResize = () => {
 			setIFrameHeight(calculateAvailableHeight());
 		};
-
 		window.addEventListener('resize', handleResize);
 		return () => {
-			return window.removeEventListener('resize', handleResize);
+			window.removeEventListener('resize', handleResize);
 		};
 	}, []);
 
-	const viewerLoad = () => {
-		setIsLoaded(true);
-	};
+	useEffect(() => {
+		// Listen for events from the iframe
+		const handleMessage = (event: MessageEvent) => {
+			// Verify origin of messages
+			if (event.origin !== 'https://sites.dlib.nyu.edu') {
+				return;
+			}
+			if (!JSON.parse(event.data)) {
+				return;
+			}
+			const parsedEvent = JSON.parse(event.data);
+			switch (parsedEvent.fire) {
+				case 'viewer:init':
+					break;
+				case 'viewer:loaded':
+					break;
+				case 'viewer:contentready':
+					setIsLoaded(true);
+					break;
+				// page changes
+				case 'viewer:sequence:change':
+					window.history.pushState({}, '', `${parsedEvent.message.sequence}`);
+					break;
+				// multivolume change
+				case 'change:option:multivolume':
+					// Check if message contains a bookid and not default message:"/1?lang=en"
+					if (parsedEvent.message.length > 10) {
+						const newPid = parsedEvent.message.slice(14, -10);
+						window.location.href = `${window.location.origin}/books/${newPid}/1`;
+					}
+					break;
+				default:
+					break;
+			}
+		};
+
+		window.addEventListener('message', handleMessage);
+
+		return () => {
+			window.removeEventListener('message', handleMessage);
+		};
+	}, []);
 
 	return (
 		<>
 			{!isLoaded && <BookviewerPlaceholder height={iFrameHeight} />}
 			<iframe
 				role="main"
-				onLoad={viewerLoad}
 				onError={(e) => {
 					return console.error('iframe failed to load:', e);
 				}}
@@ -47,6 +85,7 @@ const Book: React.FC<BookProps> = (props) => {
 				name="book"
 				allowFullScreen={true}
 				src={`${viewer}/books/${pid}`}
+				ref={iFrameRef}
 			/>
 		</>
 	);
