b9485a8f78bbd97c812602e9d96835b0af934b5d
Damon Chu <dc3800@nyu.edu>
2025-10-14 16:05:18 -0400

    empty repo

--- src/components/Header/metatags.ts
+++
@@ -1,6 +0,0 @@
-export const metatags = {
-	siteName: 'Ancient World Digital Library',
-	defaultAuthor: 'NYU Digital Library Technology Services',
-	poweredDesc: 'Powered by NYU Digital Library Technology Services',
-	type: 'website'
-};
