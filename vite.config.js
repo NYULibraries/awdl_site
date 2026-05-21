5962e491dd2ca3b14d750a3788deca239b1e2263
Damon Chu <dc3800@nyu.edu>
2026-05-21 13:10:28 -0400

    removed viteconfig for hmr,server

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,6 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    origin: 'https://awdl-site.ddev.site:5173',
-    cors: {
-      origin: 'https://awdl-site.ddev.site',
-    },
-    hmr: {
-      host: 'awdl-site.ddev.site',
-      protocol: 'wss',
-      clientPort: 5173,
-    },
-  },
   plugins: [
     laravel({
       input: ['resources/js/app.tsx', 'resources/sass/style.scss'],
